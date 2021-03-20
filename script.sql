cc8cb7ddec9108601ded6b2d0b62cb0275adf1ac
jesusromanm99 <jesusromanm99@fpuna.edu.py>
2021-03-20 12:30:51 -0300

    Creacio Tablas en Postgres

--- script.sql
+++ script.sql
@@ -1,10 +1,58 @@
-CREATE TABLE nombre, apellido, número de
-documento, tipo de documento, nacionalidad, email, teléfono, fecha de nacimiento.
+create table cliente (
+	id_cliente Serial Primary Key, 
+	nombre VARCHAR (100) NOT NULL ,
+	apellido VARCHAR (100) NOT NULL,
+	numero_documento Integer UNIQUE NOT NULL, 
+	tipo_documento VARCHAR (100) NOT NULL,
+	nacionalidad VARCHAR (100) NOT NULL,
+	email VARCHAR (100) NOT NULL,
+	teléfono Integer ,
+	fecha_nacimiento TIMESTAMP NOT NULL
+);
+create table concepto_puntos(
+	id_concepto Serial Primary Key,
+	descripcion Varchar (100) NOT NULL,
+	puntos_requeridos Integer NOT NULL
+);
+create table asignacion_puntos(
+	id_asignacion Serial Primary Key,
+	limite_inferior Integer NOT NULL,
+	limite_superior Integer NOT NULL,
+	monto Integer NOT NULL
+);
+create table vencimiento_puntos(
+	id_vencimiento Serial Primary Key,
+	fecha_inicio TIMESTAMP NOT NULL,
+	fecha_fin TIMESTAMP NOT NULL,
+	duracion Integer
+);
+create table bolsa_puntos(
+	id_bolsa Serial Primary Key,
+	id_cliente Integer NOT NULL,
+	fecha_asignacion TIMESTAMP NOT NULL,
+	fecha_caducidad TIMESTAMP NOT NULL,
+	puntajes_asignado Integer NOT NULL,
+	puntaje_utilizado Integer NOT NULL,
+	saldo_puntos Integer NOT NULL,
+	monto Integer NOT NULL,
+	FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
+);
 
- id autogenerado, descripción de concepto, puntos requeridos.
+create table uso_puntos_cabecera(
+	id_cabecera Serial Primary Key,
+	id_cliente Integer NOT NULL,
+	id_concepto Integer NOT NULL,
+	puntaje_utilizado Integer NOT NULL,
+	fecha TIMESTAMP NOT NULL,
+	FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
+	FOREIGN KEY(id_concepto) REFERENCES concepto_puntos (id_concepto)
+);
 
-  id autogenerado, limite inferior, límite superior, monto de equivalencia de 1
-punto
-
- id autogenerado, fecha de inicio de validez, fecha fin de validez, días de
-duración del puntaje.
+create table uso_puntos_detalle(
+	id_detalle Serial Primary Key,
+	id_cabecera Integer NOT NULL,
+	id_bolsa Integer NOT NULL,
+	puntaje_utilizado Integer NOT NULL,
+	FOREIGN KEY (id_cabecera) REFERENCES uso_puntos_cabecera(id_cabecera),
+	FOREIGN Key( id_bolsa) REFERENCES bolsa_puntos (id_bolsa)
+);
