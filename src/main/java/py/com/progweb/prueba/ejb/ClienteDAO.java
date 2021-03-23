b67a2d58895ec72cadc389b1205ecebf8e13fd50
jesusromanm99 <jesusromanm99@fpuna.edu.py>
2021-03-23 00:14:13 -0300

    Se agrega una funcion para obtener Clientes por Id  y se expone eso como un servicio

--- src/main/java/py/com/progweb/prueba/ejb/ClienteDAO.java
+++ src/main/java/py/com/progweb/prueba/ejb/ClienteDAO.java
@@ -1,22 +1,35 @@
 package py.com.progweb.prueba.ejb;
 
 import py.com.progweb.prueba.model.Cliente;
-import py.com.progweb.prueba.model.Persona;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
+
+@Stateless
 public class ClienteDAO {
     @PersistenceContext(unitName = "laboratiorioPersistanceUnit")
     private  EntityManager em;
-
+    // funcio para agregar un nuevo cliente
     public  void add(Cliente cliente){
         em.persist(cliente);
     }
+    //funcion para listar todos los clientes
     public List<Cliente> getAll(){
         Query query = this.em.createQuery("select c from Cliente c");
         return  (List<Cliente>)  query.getResultList();
+    }
+
+    public Cliente get(Long id_cliente){
+        Cliente cliente= this.em.find(Cliente.class,id_cliente);
+        return cliente;
+    }
+
+    public Cliente getClienteByName(String nombre) {
+        Query q= this.em.createQuery("select c from Cliente c where c.nombre=nombre");
+        return  (Cliente) q.getSingleResult();
     }
 }
--- src/main/java/py/com/progweb/prueba/rest/ClienteRest.java
+++ src/main/java/py/com/progweb/prueba/rest/ClienteRest.java
@@ -6,6 +6,7 @@ import py.com.progweb.prueba.model.Cliente;
 import py.com.progweb.prueba.model.Persona;
 
 import javax.inject.Inject;
+import javax.management.Query;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
@@ -19,10 +20,22 @@ public class ClienteRest {
 
     @GET
     @Path("/")
-    public Response listar(){
+    public Response listarClientes(){
      return  Response.ok(this.clienteDAO.getAll()).build();
     }
 
+
+    @GET
+    @Path("/id/{id_cliente}")
+    public Response getCliente(@PathParam("id_cliente") Long id_cliente){
+        return Response.ok(clienteDAO.get(id_cliente)).build();
+    }
+    @GET
+    @Path("/nombre/{nombre_cliente}")
+    public Response getClienteByName(@PathParam("nombre_cliente") String nombre ){
+        Cliente cliente= clienteDAO.getClienteByName(nombre);
+        return  Response.ok(cliente).build();
+    }
     @POST
     @Path("/")
     public  Response agregar(Cliente cliente){
